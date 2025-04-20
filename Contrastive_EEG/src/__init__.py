385a34ac8172fad312fb3cc0e734cfdfcee16522
amar3012005 <148769827+amar3012005@users.noreply.github.com>
2025-04-20 17:29:39 +0530

    Delete Contrastive_EEG directory

--- Contrastive_EEG/src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Contrastive EEG-to-Text Model
-
-This package implements a contrastive learning approach for aligning EEG signals
-with their corresponding textual representations using the ZuCo dataset.
-"""
-
-__version__ = '0.1.0'
